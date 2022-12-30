fd93b2951d879ab6e1451f60f3a9013176133d2c
David K. Tan <32520157+skytruong90@users.noreply.github.com>
2022-12-29 21:46:27 -0500

    Add files via upload

---
+++ src/main/java/levvel/io/controller/BlogController.java
@@ -0,0 +1,27 @@
+package levvel.io.controller;
+
+import levvel.io.model.Blog;
+import levvel.io.service.BlogService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/blog")
+public class BlogController {
+
+    private BlogService blogService;
+
+    @PostMapping("/post")
+    public ResponseEntity<Blog> addBlog(@RequestBody Blog blog) {
+        blogService.addBlog(blog);
+        return ResponseEntity.ok().body(blog);
+    }
+
+    @GetMapping("/post/{id}")
+    public ResponseEntity<Blog> getBlog(@PathVariable String id) {
+        Blog blog = blogService.getBlog(id);
+        return ResponseEntity.ok().body(blog);
+    }
+}
